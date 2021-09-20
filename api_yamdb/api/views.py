6fb1cc376cb0e5c0a95240d635fa78faf1367553
priest4612 <prie4612@gmail.com>
2021-09-20 18:21:46 +0300

    Удалил лишние комментарии

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -36,9 +36,6 @@ class ReviewViewSet(viewsets.ModelViewSet):
         title = get_object_or_404(Title, pk=title_id)
         serializer.is_valid(raise_exception=True)
         serializer.save(author=self.request.user, title=title)
-        # title.score = (Review.objects.filter(title=title).aggregate(Avg(
-        #     'score'))['score__avg'])
-        # title.save(update_fields=['score'])
 
     def perform_create(self, serializer):
         self.save_review(serializer)
