d2e0a6f9a73dd9cc398fd2db362a51a5d07e1c10
priest4612 <prie4612@gmail.com>
2021-09-20 15:07:39 +0300

    Добавил индексацию БД

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -21,7 +21,7 @@ class Genre(models.Model):
 
 
 class Title(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, db_index=True)
     year = models.IntegerField()
     description = models.TextField()
     genre = models.ManyToManyField(Genre)
